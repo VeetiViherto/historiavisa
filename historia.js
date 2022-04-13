f9adb5e4d88a6f290fc56c5a0d13b94f9dc7eee8
VeetiViherto <101732081+VeetiViherto@users.noreply.github.com>
2022-04-13 19:36:22 +0300

    commit

---
+++ historia.js
@@ -0,0 +1,9 @@
+function myFunction (){
+  let one=  document.getElementById("HistoriaEnsimmäinen").value
+  let two=  document.getElementById("HistoriaToinen").value
+  let three=  document.getElementById("HistoriaKolmas").value
+  let four=  document.getElementById("HistoriaNeljäs").value
+  
+ 
+    
+}
